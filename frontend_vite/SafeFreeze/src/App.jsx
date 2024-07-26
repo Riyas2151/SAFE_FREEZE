a15fae2743d5165907dcdb494a39c4c27af8bdf2
Riyas2151 <165140547+Riyas2151@users.noreply.github.com>
2024-07-26 04:04:23 +0000

    tailwing setup done

--- frontend_vite/SafeFreeze/src/App.jsx
+++ frontend_vite/SafeFreeze/src/App.jsx
@@ -4,8 +4,11 @@ function App() {
     //jo yaha likhuga vo website pe dikhega.
     <>
     <h1>Radhe Radhe</h1>
+    <div className="w-full h-screen bg-zinc-800">
+      Hello world!
+    </div>
     </>
- )
+ );
 }
 
 export default App
