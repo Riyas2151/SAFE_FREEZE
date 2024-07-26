df7c9752d20aa09da6c88b51a3aff39a81af9013
Riyas2151 <165140547+Riyas2151@users.noreply.github.com>
2024-07-26 06:28:59 +0000

    start components

--- frontend_vite/SafeFreeze/src/App.jsx
+++ frontend_vite/SafeFreeze/src/App.jsx
@@ -1,14 +1,17 @@
-import React from "react"
+import React  from 'react'
+import Nav from './Components/Nav';
+
 function App() {
+  // var [a,b]= useState(22);
   return (
-    //jo yaha likhuga vo website pe dikhega.
     <>
-    <h1>Radhe Radhe</h1>
-    <div className="w-full h-screen bg-zinc-800">
-      Hello world!
-    </div>
+    <Nav/>
+      {/* <div className="w-full h-screen bg-zinc-700 text-white">
+        <h1 className='text-2xl '>Hello Let's Start</h1> */}
+        {/* </div> */}
+        {/* <Sign/> */}
     </>
- );
+  )
 }
 
 export default App
