01e801bae01f460eafc63c06b3363052733a8fed
Riyas2151 <165140547+Riyas2151@users.noreply.github.com>
2024-08-07 08:47:51 +0000

    Navar done

--- frontend_vite/safe/src/components/Nav.jsx
+++ frontend_vite/safe/src/components/Nav.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 function Nav() {
   return (
-    <div className="w-full px-20 py-8 font-['Neue Montreal']" >
+    <div className=" fixed z-[999] w-full px-20 py-8 font-['Neue Montreal'] flex justify-between items-center" >
         <div className="logo">
-             <img src="/3.png" className="h-10 w-auto" alt="Logo" />                  
+             <img src="/logo.png" className="h-10 w-auto" alt="Logo" />                  
         </div>
-        <div className="links flex gap-4">
-          {["Services","Our Works","About Us","Contact Us"].map((item,index)=>(
-          <a key={index} className='text-md font-light'>{item} </a>))}
+        <div className="links flex gap-10">
+          {["Home","Services","Our Works","About Us","contact Us"].map((item,index)=>(
+          <a key={index} className={`text-lg font-light capitalize ${index === 4 && "ml-32"}`}>{item} </a>))}
+          {/* to seprate contact us we use dynamic jsx we use classname in jsx brackets alow us to use js */}
         </div>
     </div>
   )
